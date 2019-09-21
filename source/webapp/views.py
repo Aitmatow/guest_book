0c45bff8247d162acaf87b08c58d43a7dc4e2d87
Diiar <aitmarowd@gmail.com>
2019-09-21 12:50:55 +0600

    Добавил иконки для добавления, редактирования а так-же фильтрацию по активным записям и сортировку по датам в главной форме (Этап 2 заверщен).

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from webapp.models import Records
 
 def records_index_view(request, *args, **kwargs):
-        records = Records.objects.all()
+        records = Records.objects.all().filter(status='active').order_by('-create_date')
         return render(request, 'index.html', context={
             'records' : records
         })
